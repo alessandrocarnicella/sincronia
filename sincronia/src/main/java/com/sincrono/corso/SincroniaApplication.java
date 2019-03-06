666c2994d123869aff23e138f30e54f6f35e305e
Alessandro <alessandro.9403@hotmail.it>
2019-03-06 14:27:54 +0100

    1° Commit

---
+++ sincronia/src/main/java/com/sincrono/corso/SincroniaApplication.java
@@ -0,0 +1,13 @@
+package com.sincrono.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SincroniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SincroniaApplication.class, args);
+	}
+
+}
---
+++ sincronia/src/main/java/com/sincrono/corso/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.sincrono.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/")
+	public String getIndex() {
+		return "Login";
+	}
+	
+	@RequestMapping("/Prova")
+	public String getHello() {
+		return "Prova";
+	}
+
+}
